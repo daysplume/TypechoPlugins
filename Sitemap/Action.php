faca7eabe541e645785e092bb7a98e83f3ecf561
yutuo <yutuo5@gmail.com>
2019-02-10 17:58:29 +0900

    禁止Page

--- Sitemap/Action.php
+++ Sitemap/Action.php
@@ -6,35 +6,35 @@ class Sitemap_Action extends Typecho_Widget implements Widget_Interface_Do
 		$db = Typecho_Db::get();
 		$options = Typecho_Widget::widget('Widget_Options');
 
-		$pages = $db->fetchAll($db->select()->from('table.contents')
-		->where('table.contents.status = ?', 'publish')
-		->where('table.contents.created < ?', $options->gmtTime)
-		->where('table.contents.type = ?', 'page')
-		->order('table.contents.created', Typecho_Db::SORT_DESC));
+//		$pages = $db->fetchAll($db->select()->from('table.contents')
+//		->where('table.contents.status = ?', 'publish')
+//		->where('table.contents.created < ?', $options->gmtTime)
+//		->where('table.contents.type = ?', 'page')
+//		->order('table.contents.created', Typecho_Db::SORT_DESC));
 
 		$articles = $db->fetchAll($db->select()->from('table.contents')
 		->where('table.contents.status = ?', 'publish')
 		->where('table.contents.created < ?', $options->gmtTime)
 		->where('table.contents.type = ?', 'post')
-		->order('table.contents.created', Typecho_Db::SORT_DESC));
+		->order('table.contents.modified', Typecho_Db::SORT_DESC));
 
 		header("Content-Type: application/xml");
 		echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 		echo "<?xml-stylesheet type='text/xsl' href='" . $options->pluginUrl . "/Sitemap/sitemap.xsl'?>\n";
 		echo "<urlset xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\nxsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\"\nxmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
-		foreach($pages AS $page) {
-			$type = $page['type'];
-			$routeExists = (NULL != Typecho_Router::get($type));
-			$page['pathinfo'] = $routeExists ? Typecho_Router::url($type, $page) : '#';
-			$page['permalink'] = Typecho_Common::url($page['pathinfo'], $options->index);
-
-			echo "\t<url>\n";
-			echo "\t\t<loc>".$page['permalink']."</loc>\n";
-			echo "\t\t<lastmod>".date('Y-m-d\TH:i:s\Z',$page['modified'])."</lastmod>\n";
-			echo "\t\t<changefreq>always</changefreq>\n";
-			echo "\t\t<priority>0.8</priority>\n";
-			echo "\t</url>\n";
-		}
+//		foreach($pages AS $page) {
+//			$type = $page['type'];
+//			$routeExists = (NULL != Typecho_Router::get($type));
+//			$page['pathinfo'] = $routeExists ? Typecho_Router::url($type, $page) : '#';
+//			$page['permalink'] = Typecho_Common::url($page['pathinfo'], $options->index);
+//
+//			echo "\t<url>\n";
+//			echo "\t\t<loc>".$page['permalink']."</loc>\n";
+//			echo "\t\t<lastmod>".date('Y-m-d\TH:i:s\Z',$page['modified'])."</lastmod>\n";
+//			echo "\t\t<changefreq>always</changefreq>\n";
+//			echo "\t\t<priority>0.8</priority>\n";
+//			echo "\t</url>\n";
+//		}
 		foreach($articles AS $article) {
 			$type = $article['type'];
 			$article['categories'] = $db->fetchAll($db->select()->from('table.metas')
